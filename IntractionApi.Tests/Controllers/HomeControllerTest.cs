e84c5ff2a9f42e0453f9ba6d30874df65f5076f9
vinod <vinod991@gmail.com>
2017-07-03 17:53:41 +0530

    Added Test Methods

--- IntractionApi.Tests/Controllers/HomeControllerTest.cs
+++ IntractionApi.Tests/Controllers/HomeControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace IntractionApi.Tests.Controllers
 {
     public class HomeControllerTest
     {
+        public void TestHomeControllerNewBusiness()
+        {
+            var controller = new IntractionApi.Controllers.HomeController();
+            controller.Request = new System.Net.Http.HttpRequestMessage();
+            controller.Configuration = new System.Web.Http.HttpConfiguration();
+            var response = controller.NewBusiness();
+            //Assert.AreEqual("1", response.)
+        }
+
     }
 }
